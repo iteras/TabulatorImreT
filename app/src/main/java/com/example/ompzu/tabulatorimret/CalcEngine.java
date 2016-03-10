65020a0b6668c4978ea6e85a920b923726f0afbf
Immu <teras_8@hotmail.com>
2016-03-10 17:30:55 +0200

    all operations added and all works, but has bugs: numbers go out of screen, multiple operations can be inserted on so on

--- app/src/main/java/com/example/ompzu/tabulatorimret/CalcEngine.java
+++ app/src/main/java/com/example/ompzu/tabulatorimret/CalcEngine.java
@@ -1,15 +1,25 @@
 package com.example.ompzu.tabulatorimret;
 
 import java.util.ArrayList;
+import java.math.*;
 
 /**
  * Created by Ompzu on 3/9/2016.
  */
 public class CalcEngine {
     private static final String TAG = "CalcEngine";
-    private static final String[] op = {"c", "x","d","E", "P", "buttonMinus" };
+    public static final String[] op = {"C", "X","D","E", "P", "M", "N", "S","W","K" };
    // public ArrayList<String> input = new ArrayList<String>();
 
+    public static int compare(String str){
+        int result = 9; //random number, must not be 0 at start
+        for(int i = 0; i < 6; i++){
+            if(str.compareTo(op[i]) == 0){ //the string inserted is one of the operations
+                result = 0;
+            }
+        }
+        return result;
+    }
 
     public static String numerics(){
         String a = "";
@@ -21,22 +31,39 @@ public class CalcEngine {
 
     public static ArrayList<String> operation(ArrayList<String> in){
         double result = 0;
-        double a = Double.parseDouble(in.get(0));
-        double b = Double.parseDouble(in.get(2));
+        double a,b = 0;
+        try {
+            a = Double.parseDouble(in.get(0));
+        } catch ( Exception e) {
+            a = Double.parseDouble(in.get(1));
+        }
+        // a = Double.parseDouble(in.get(0));
 
+        if(in.size() > 2){ //if given equation is not sin or cos
+            b = Double.parseDouble(in.get(2));
+        }
 
 
         if(in.contains("X")){ //multiply
             result = times(a, b);
         }
         if(in.contains("D")){ //divide
-            result = divide(a,b);
+            result = divide(a, b);
         }
         if(in.contains("P")){ // plus
-            result = plus(a,b);
+            result = plus(a, b);
         }
         if(in.contains("M")){ //minus
             result = minus(a, b);
+        }
+        if(in.contains("W")){
+            result = Math.pow(a, b);
+        }
+        if(in.contains("N")){
+            result = Math.sin(a);
+        }
+        if(in.contains("S")){
+            result = Math.cos(a);
         }
         in.clear();
         in.add(String.valueOf(result));
--- app/src/main/java/com/example/ompzu/tabulatorimret/MainActivity.java
+++ app/src/main/java/com/example/ompzu/tabulatorimret/MainActivity.java
@@ -10,14 +10,20 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
+
 import com.example.ompzu.tabulatorimret.CalcEngine;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> input = new ArrayList<String>();
 
+    private ArrayList<String> input = new ArrayList<String>();
+    private TextView textViewResult;
     public static String nr = "";
+    private static String showResult="";
+    private static String showEquation="";
+    private static String btnId="";
     private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,50 +32,101 @@ public class MainActivity extends AppCompatActivity {
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "onCreate called");
         }
-
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
     }
 
 
     public void buttonClicked(View view){
         Button btn = (Button) view;
 
-        String btnId = btn.getResources().getResourceName(btn.getId());
-        btnId = btnId.substring(btnId.length() - 1, btnId.length());
-
-        if(!btnId.equals("P") && !btnId.equals("M") && !btnId.equals("X") &&
-                !btnId.equals("D") && !btnId.equals("C") && !btnId.equals("E") ){ //input is number
-            nr = nr + btnId; //click 7 nr is 7, click 4 and nr is 74, click 1 nr is 741
+         btnId = btn.getResources().getResourceName(btn.getId());
+         btnId = btnId.substring(btnId.length() - 1, btnId.length());
+       /* if(mem.isEmpty()){
+            btnId = btnId.substring(btnId.length() - 1, btnId.length());
+            mem = btnId;
+        } else {
+            btnId = mem;
         }
+*/
 
-        if (btnId.equals("P") || btnId.equals("M") || btnId.equals("X") || btnId.equals("D")) { //input is operation
-            input.add(nr);
-            nr = ""; //remove number from memory
-            input.add(btnId);
-        }
 
         if(btnId.equals("E")){ //input is equals
-            input.add(nr);
-            nr = "";//remove number from memory
+            if(!nr.isEmpty()){
+                input.add(nr);  //operation inserted, finsih the nr, add to arraylist and clean the string
+            } else {
+                nr = "";//remove number from memory
+            }
             if(input.size() < 3) input.indexOf(0); //TODO// in finishing, send result to displayscreen
         }
+
+        if(!btnId.equals("P") && !btnId.equals("M") && !btnId.equals("X") &&
+                !btnId.equals("D") && !btnId.equals("C") && !btnId.equals("E") &&
+                !btnId.equals("W") && !btnId.equals("N") && !btnId.equals("S")){ //input is number
+            if(btnId.equals("K") && !input.contains(".")){
+                btnId = ".";
+                nr = nr + btnId;
+            } else {
+                nr = nr + btnId; //click 7 nr is 7, click 4 and nr is 74, click 1 nr is 741
+                showEquation = showEquation + btnId;
+            }
+
+        }
+
+        if (btnId.equals("P") || btnId.equals("M") || btnId.equals("X") || btnId.equals("D") ||
+                btnId.equals("W") || btnId.equals("N") || btnId.equals("S") ) { //input is operation
+            if(!nr.isEmpty()){
+                input.add(nr); //operation inserted, finsih the nr, add to arraylist and clean the string
+            }
+            if(input.size() == 1 && input.size() < 3){
+                nr = ""; //remove number from memory
+                showEquation = showEquation + btn.getText().toString();
+                input.add(btnId);
+            }
+
+        }
+
+
 
         if(btnId.contains("C")) { //input is Clear function
             input.clear();
             nr = "";//remove number from memory
+            showResult ="";
+            showEquation="";
             if(BuildConfig.DEBUG) Log.d( TAG,"Array reset: " + input.toString());
         }
-
+        int firstOsCheck = 0;
+        int secondOsCheck= 0;
        // input.add(nr);
-        if(input.size() == 3 && input.get(0).contains(CalcEngine.numerics()) //TODO
-                &&(input.contains("P") || input.contains("M") || input.contains("X") || input.contains("D"))){
-           input = CalcEngine.operation(input); //the operation will be done and equation will be calculated
+        if(input.size() > 0){
+            firstOsCheck = CalcEngine.compare(input.get(0)); //returns 0 if string in arraylist slot equals to operation, else its number
         }
+
+        if(input.size() == 3){
+            secondOsCheck = CalcEngine.compare(input.get(2)); //returns 0 if string in arraylist slot equals to operation, else its number
+        }
+
+            if(input.size() == 3 && firstOsCheck != 0 && secondOsCheck != 0
+                    &&(input.contains("P") || input.contains("M") || input.contains("X") ||
+                    input.contains("D") || input.contains("W"))){
+                input = CalcEngine.operation(input); //the operation will be done and equation will be calculated
+        } else if(input.size() == 2 && (btnId.contains("S") ||btnId.contains("N"))){
+                input = CalcEngine.operation(input); //the operation will be done and equation will be calculated
+            }
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "Button clicked: " + btnId + " in array: " + input.toString());
-
-
         }
 
+        if(input.size() == 1){
+            showResult = input.get(0);
+        }
+
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
+        if(btnId.equals("E")) {
+            textViewResult.setText( showEquation = showEquation + " = " + showResult);
+        } else  {
+
+            textViewResult.setText(showEquation + " ");
+        }
 
     }
 
@@ -86,6 +143,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         // The activity has become visible (it is now "resumed").
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
+        textViewResult.setText(showEquation);
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "onResume called");
         }
